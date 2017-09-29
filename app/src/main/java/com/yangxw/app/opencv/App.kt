4c0f0024438ebe09b7feaaaf70b09e5b4ee4d53f
yangxw <yangxw8820@gmail.com>
2017-09-29 23:17:12 +0800

    app,permission

---
+++ app/src/main/java/com/yangxw/app/opencv/App.kt
@@ -0,0 +1,14 @@
+package com.yangxw.app.opencv
+
+import android.app.Application
+
+/**
+ * Created by YCL on 2017/9/29.
+ */
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
